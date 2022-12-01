eca9450cb74d15ff837825f4ba31b3cf470c42d2
Azat <8280770+azataiot@users.noreply.github.com>
2022-12-01 20:50:19 +0300

    finish this part

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
@@ -31,3 +32,32 @@ def update_item_sync(item_id: int, item: Item):
     Update a single item synchronously
     """
     return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
+
+
+# =================== Path Parameters | Order Matters =====================
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": 1, "user_name": "Current username"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id, "user_name": "requested username for {}".format(user_id)}
+
+
+# =================== Path Parameters | Predefined Values =================
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = 'resnet'
+    lenet = 'lenet'
+
+
+@app.get("/models/{model_name}")
+async def read_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW"}
+    elif model_name.value == 'lenet':
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
