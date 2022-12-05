61efdc59ab7b5d2d0995a1c534c9d31fa240f256
Azat <8280770+azataiot@users.noreply.github.com>
2022-12-05 18:39:16 +0300

    finished this

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from datetime import datetime, time, timedelta
 from typing import Union
-from fastapi import FastAPI, Query, Path, Body
+from uuid import UUID
+
+from fastapi import FastAPI, Query, Path, Body, Cookie
 from pydantic import BaseModel, Field, HttpUrl
 from enum import Enum
 
@@ -337,3 +340,33 @@ class Item9(BaseModel):
 async def update_item8(item_id: int, item: Item9):
     results2 = {"item_id": item_id, "item": item}
     return results2
+
+
+# ================== Extra Data Types =============================
+@app.put("/items10/{item_id}")
+async def read_items(
+        item_id: UUID,
+        start_datetime: datetime | None = Body(default=None),
+        end_datetime: datetime | None = Body(default=None),
+        repeat_at: time | None = Body(default=None),
+        process_after: timedelta | None = Body(default=None),
+):
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration,
+    }
+
+
+# ========= Cookie Parameters =========
+
+@app.get("/items11/")
+async def read_items11(ads_id: str | None = Cookie(default=None)):
+    print(ads_id)
+    return {"ads_id": ads_id}
