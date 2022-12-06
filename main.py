f9ef5d8c59ca55a78ae4efc06f65288917a98ba6
Azat <8280770+azataiot@users.noreply.github.com>
2022-12-06 21:42:06 +0300

    finished this

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime, time, timedelta
 from typing import Union
 from uuid import UUID
 
-from fastapi import FastAPI, Query, Path, Body, Cookie, Header, Form, File, UploadFile
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header, Form, File, UploadFile, Depends
 from pydantic import BaseModel, Field, HttpUrl, EmailStr, SecretStr
 from enum import Enum
 
@@ -491,3 +491,35 @@ async def create_file(
         "token": token,
         "fileb_content_type": fileb.content_type,
     }
+
+
+# ============== Dependencies ==============
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/items-depends/")
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+@app.get("/users-depends/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items-common-depends/")
+async def read_items(commons: CommonQueryParams = Depends(CommonQueryParams)):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip: commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
