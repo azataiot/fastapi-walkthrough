7ba8fab2dccadf8f1957d7c277acda28567e6107
Azat <8280770+azataiot@users.noreply.github.com>
2022-12-07 14:32:22 +0300

    finished this

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime, time, timedelta
 from typing import Union
 from uuid import UUID
 
-from fastapi import FastAPI, Query, Path, Body, Cookie, Header, Form, File, UploadFile, Depends
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header, Form, File, UploadFile, Depends, HTTPException
 from pydantic import BaseModel, Field, HttpUrl, EmailStr, SecretStr
 from enum import Enum
 
@@ -523,3 +523,76 @@ async def read_items(commons: CommonQueryParams = Depends(CommonQueryParams)):
     items = fake_items_db[commons.skip: commons.skip + commons.limit]
     response.update({"items": items})
     return response
+
+
+def query_extractor(q: str | None = None):
+    return q
+
+
+def query_or_cookie_extractor(
+        q: str = Depends(query_extractor), last_query: str | None = Cookie(default=None)
+):
+    # If the user didn't provide any query q, we use the last query used, which we saved to a cookie before.
+    if not q:
+        return last_query
+    return q
+
+
+@app.get("/items12/")
+async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
+    return {"q_or_cookie": query_or_default}
+
+
+# Dependency in the path operation decorator
+async def verify_token(x_token: str = Header()):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: str = Header()):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
+@app.get("/items12/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
+
+
+# Dependency with yield
+
+async def generate_dep_a():
+    pass
+
+
+async def generate_dep_b():
+    pass
+
+
+async def generate_dep_c():
+    pass
+
+
+async def dependency_a():
+    dep_a = generate_dep_a()
+    try:
+        yield dep_a
+    finally:
+        dep_a.close()
+
+
+async def dependency_b(dep_a=Depends(dependency_a)):
+    dep_b = generate_dep_b()
+    try:
+        yield dep_b
+    finally:
+        dep_b.close(dep_a)
+
+
+async def dependency_c(dep_b=Depends(dependency_b)):
+    dep_c = generate_dep_c()
+    try:
+        yield dep_c
+    finally:
+        dep_c.close(dep_b)
