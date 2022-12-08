2f3598ad661ca108a659a4e310c4323922957946
Azat <8280770+azataiot@users.noreply.github.com>
2022-12-08 21:17:52 +0300

    finished this

---
+++ app_metadata_docs/main2.py
@@ -0,0 +1,36 @@
+# app_metadata_docs / main2.py
+# Created by azat at 8.12.2022
+# Create metadata for tags¶
+# Let's try that in an example with tags for users and items.
+#
+# Create metadata for your tags and pass it to the openapi_tags parameter:
+
+from fastapi import FastAPI
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
+
+
+@app.get("/users/", tags=["users"])
+async def get_users():
+    return [{"name": "Harry"}, {"name": "Ron"}]
+
+
+@app.get("/items/", tags=["items"])
+async def get_items():
+    return [{"name": "wand"}, {"name": "flying broom"}]
+
