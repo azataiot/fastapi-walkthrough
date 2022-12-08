e55daab16b690a84e3047c80549611120ab61ece
Azat <8280770+azataiot@users.noreply.github.com>
2022-12-08 20:54:15 +0300

    finished this

---
+++ app/internal/admin.py
@@ -0,0 +1,11 @@
+# app/internal / admin.py
+# Created by azat at 8.12.2022
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.post("/")
+async def update_admin():
+    return {"message": "Admin getting schwifty"}
